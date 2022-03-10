3724f1ff864e367e6bed245e089bf8c52bc2bc9b
himanshubawankar <98639879+himanshubawankar@users.noreply.github.com>
2022-03-10 22:38:25 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Mock Test</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<div class="">
+<nav class="navbar navbar-expand-sm navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand fw-bold" href="javascript:void(0)">Mock Test</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="  collapse navbar-collapse mx-5  " id="mynavbar">
+      <ul class="navbar-nav me-auto ">
+        <li class="nav-item ">
+          <a class="nav-link px-5 fw-bold" href="javascript:void(0)">Home</a>
+        </li>
+        <li class="nav-item ">
+          <a class="nav-link px-5 fw-bold" href="javascript:void(0)">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link px-5 fw-bold" href="javascript:void(0)">Contact</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <button class="btn btn-warning px-3 fw-bold" type="button">Sign in</button>
+        <button class="btn btn-dark mx-2 px-3 fw-bold" type="button">Sign up</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<div class="container-fluid mt-3">
+  <h3>Navbar Forms</h3>
+  <p>You can also include forms inside the navigation bar.</p>
+</div>
+</div>
+</body>
+</html>
+
+
